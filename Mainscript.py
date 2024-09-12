ad4cb66d29fbc4d49eff81381fa07940a6c334a2
Kostin Kirill <kostink.rrj@gmail.com>
2024-09-12 14:03:53 +0300

    Import

---
+++ Mainscript.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import psutil
+import subprocess
+import re
+import logging
+import random
+from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
