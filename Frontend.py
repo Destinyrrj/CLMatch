6513ca360f5272753204c53569ad520dee583c2b
Kostin Kirill <kostink.rrj@gmail.com>
2024-09-12 14:57:49 +0300

    l

--- Frontend.py
+++ Frontend.py
@@ -8,10 +8,16 @@ from PySide6 import QtWidgets, QtCore, QtGui
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWidgets import QApplication, QLabel
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot, Qt
 import platform
 import logging
 import threading
 import time
 import sphinx
 
+if __name__ == "__main__":
+    app = QApplication()
+    w = QLabel("This is a placeholder text")
+    w.setAlignment(Qt.AlignCenter)
+    w.show()
+    sys.exit(app.exec())
